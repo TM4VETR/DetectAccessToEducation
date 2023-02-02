12c2bc9050149266cecfb40dbeadffb1d1899473
Jens <>
2023-02-02 14:15:50 +0100

    files

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # DetectionAcessToEducation
 A novel approach to- wards the automated detection of access to education and training in German training offers and advertisements.
+
+# Background
+
+The German labour market heavily relies on vocational education and training, re-training and advanced vocational qualification. To match training seekers and offers, we present a novel approach towards the automated detection of access to education and training in German training offers and advertisements. We will in particular focus on (a) general school and education degrees and school-leaving certificates, (b) professional experience, (c) a previous apprenticeship and (d) a list of skills.
+
+# Installation and Usage
+
+1) Clone/Download this repository.
+2) Navigate to the folder on your machine using the commandline tool
+3) (optional) create a new python environment
+4) (python 3.8X required) Enter the following command into your commandline
+
+```bash
+pip install -r requirements.txt
+```
+
+5) start the tutorial notebook by entering the following command
+
+```bash
+jupyter-notebook
+```
+
+# Contact, Supporters, Contributions and Contributing
+
+If you have questions, please use the GitHub discussions feature at
+https://github.com/TM4VETR/DetectionAcessToEducation/issues/new.
---
+++ trainingaccessdetection.py
@@ -0,0 +1,46 @@
+import pickle
+import spacy
+
+nlp = spacy.load("de_core_news_sm")
+
+file = open("data/m1.map",'rb')
+matcher = pickle.load(file)
+file.close()
+file = open("data/m2.map",'rb')
+matcher2 = pickle.load(file)
+file.close()
+file = open("data/m3.map",'rb')
+matcher4 = pickle.load(file)
+file.close()
+file = open("data/mv.map",'rb')
+voc = pickle.load(file)
+file.close()
+
+def accessdetection(text):
+    doc = nlp(text.replace("Schulabschluss","Hauptschulabschluss"))
+    retrieved = []
+    matches = matcher4(doc)
+    for match_id, start, end in matches:
+        rule_id = voc[match_id]  # get the unicode ID, i.e. 'COLOR'
+        retrieved.append(rule_id)
+    matches = matcher(doc)
+    for match_id, start, end in matches:
+        rule_id = voc[match_id]  # get the unicode ID, i.e. 'COLOR'
+        retrieved.append(rule_id)
+    matches = matcher2(doc)
+    for match_id, start, end in matches:
+        rule_id = voc[match_id]
+        #if not "Vorbereitung auf" in wb[rule_id]:
+        retrieved.append(rule_id)
+    retrieved = list(set(retrieved))
+    if "X" in retrieved:
+        retrieved.remove("X")
+    if "B-" in retrieved:
+        if "B+" in retrieved:
+            retrieved.remove("B+")
+        retrieved.remove("B-")
+    if "BE-" in retrieved:
+        if "BE" in retrieved:
+            retrieved.remove("BE")
+        retrieved.remove("BE-")
+    return (retrieved)
